2439f1d58259a60655477e7e5c259a15f4c6af93
Vinissil <viniss-starwars@hotmail.com>
2023-07-13 22:36:16 -0300

    finalizando ladding page

--- ProjetoCSS-inicio/script.js
+++
@@ -1,6 +0,0 @@
-$(funcion(){
-
-    const token = "IGQVJVQ3UwcEduZAHR1dnNka1ZAON01xNVMxeHlhTWFQV1Buam1PZA2tSdHdCUnRoRFZA5YWdJOGdZAaGtCSXdMZAEZAtMkUxYUJlMTBwTGxUZADd2N29TaGwybGlwWHU5WS1kWllqdjlCMVhn";
-    const url = "https://graph.instagram.com/me/media?access_tokenb"+token"&field=media_url,media_type,caption,permalink";
-
-});
